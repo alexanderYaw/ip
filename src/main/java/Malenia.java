15966701b25fee8129d4db36d36f9d54a7679d42
alexanderYaw <alexanderyaw11@gmail.com>
2026-01-28 13:59:50 +0800

    Level-0: Renamed chatbot, changed greeting and exit message

---
+++ src/main/java/Malenia.java
@@ -0,0 +1,11 @@
+public class Malenia {
+    public static void main(String[] args) {
+        String greetingMsg = "_________________________________\n" +
+                      "Hello! I'm Malenia\n" +
+                      "What can I do for you?\n" +
+                      "_________________________________\n" +
+                      "Bye. Hope to see you again soon!\n" +
+                      "_________________________________";
+        System.out.println(greetingMsg);
+    }
+}
