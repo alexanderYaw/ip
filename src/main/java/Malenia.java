5b74c5e2ba69dd01cd51692285d6a977fa9147da
alexanderYaw <alexanderyaw11@gmail.com>
2026-02-11 15:28:02 +0800

    Incorporated custom Exception errors to the main code

--- src/main/java/Malenia.java
+++ src/main/java/Malenia.java
@@ -12,77 +12,85 @@ public class Malenia {
         while (!(userInput = scanner.nextLine()).equals("bye")) {
             parsedInput = userInput.split(" ");
             command = parsedInput[0];
+            try {
+                switch (command) {
+                case "list":
+                    if (!CommandCheck.isList(parsedInput)) {
+                        break;
+                    }
 
-            switch (command) {
-            case "list":
-                if (!CommandCheck.isList(parsedInput)) {
+                    taskList.printList();
                     break;
-                }
 
-                taskList.printList();
-                break;
+                case "mark":
+                    if (!CommandCheck.isMarkUnmark(parsedInput, taskList)) {
+                        break;
+                    }
 
-            case "mark":
-                if (!CommandCheck.isMarkUnmark(parsedInput, taskList)) {
+                    int indexToMark = Integer.parseInt(parsedInput[1]) - 1;
+                    taskList.getTask(indexToMark).setMarkedStatus(true);
+                    Messages.markedResponse(taskList.getTask(indexToMark));
                     break;
-                }
 
-                int indexToMark = Integer.parseInt(parsedInput[1]) - 1;
-                taskList.getTask(indexToMark).setMarkedStatus(true);
-                Messages.markedResponse(taskList.getTask(indexToMark));
-                break;
+                case "unmark":
+                    if (!CommandCheck.isMarkUnmark(parsedInput, taskList)) {
+                        break;
+                    }
 
-            case "unmark":
-                if (!CommandCheck.isMarkUnmark(parsedInput, taskList)) {
+                    int indexToUnmark = Integer.parseInt(parsedInput[1]) - 1;
+                    taskList.getTask(indexToUnmark).setMarkedStatus(false);
+                    Messages.unmarkedResponse(taskList.getTask(indexToUnmark));
                     break;
-                }
 
-                int indexToUnmark = Integer.parseInt(parsedInput[1]) - 1;
-                taskList.getTask(indexToUnmark).setMarkedStatus(false);
-                Messages.unmarkedResponse(taskList.getTask(indexToUnmark));
-                break;
+                case "todo":
+                    if (!CommandCheck.isTodo(parsedInput)) {
+                        break;
+                    }
 
-            case "todo":
-                if (!CommandCheck.isTodo(parsedInput)) {
+                    String taskName = userInput.substring(5);
+                    Todo todoTask = new Todo(taskName);
+                    taskList.add(todoTask);
+                    Messages.addTaskResponse(todoTask);
                     break;
-                }
 
-                String taskName = userInput.substring(5);
-                Todo todoTask = new Todo(taskName);
-                taskList.add(todoTask);
-                Messages.addTaskResponse(todoTask);
-                break;
+                case "deadline":
+                    if (!CommandCheck.isDeadline(parsedInput)) {
+                        break;
+                    }
 
-            case "deadline":
-                if (!CommandCheck.isDeadline(parsedInput)) {
+                    String[] deadlineParts = userInput.substring(9).split(" /by ");
+                    String deadlineName = deadlineParts[0];
+                    String dueBy = deadlineParts[1];
+                    Deadline deadlineTask = new Deadline(deadlineName, dueBy);
+                    taskList.add(deadlineTask);
+                    Messages.addTaskResponse(deadlineTask);
                     break;
-                }
 
-                String[] deadlineParts = userInput.substring(9).split(" /by ");
-                String deadlineName = deadlineParts[0];
-                String dueBy = deadlineParts[1];
-                Deadline deadlineTask = new Deadline(deadlineName, dueBy);
-                taskList.add(deadlineTask);
-                Messages.addTaskResponse(deadlineTask);
-                break;
+                case "event":
+                    if (!CommandCheck.isEvent(parsedInput)) {
+                        break;
+                    }
 
-            case "event":
-                if (!CommandCheck.isEvent(parsedInput)) {
+                    String[] eventParts = userInput.substring(6).split(" /from | /to ");
+                    String eventName = eventParts[0];
+                    String eventStartTime = eventParts[1];
+                    String eventEndTime = eventParts[2];
+                    Event eventTask = new Event(eventName, eventStartTime, eventEndTime);
+                    taskList.add(eventTask);
+                    Messages.addTaskResponse(eventTask);
                     break;
+
+                default:
+                    throw MaleniaExceptions.invalidCommandException();
                 }
+            }
 
-                String[] eventParts = userInput.substring(6).split(" /from | /to ");
-                String eventName = eventParts[0];
-                String eventStartTime = eventParts[1];
-                String eventEndTime = eventParts[2];
-                Event eventTask = new Event(eventName, eventStartTime, eventEndTime);
-                taskList.add(eventTask);
-                Messages.addTaskResponse(eventTask);
-                break;
+            catch (MaleniaExceptions e) {
+                Messages.wrapWithBars(e.getMessage());
+            }
 
-            default:
-                Messages.wrapWithBars("Invalid command! Please try again.");
-                break;
+            catch (IllegalArgumentException e) {
+                Messages.wrapWithBars(e.getMessage());
             }
         }
 
