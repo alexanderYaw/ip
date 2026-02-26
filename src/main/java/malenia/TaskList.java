78d8e3fad290a24e7f47f474004672df4e286747
alexanderYaw <alexanderyaw11@gmail.com>
2026-02-27 00:16:19 +0800

    change task list from array to ArrayList

--- src/main/java/malenia/TaskList.java
+++ src/main/java/malenia/TaskList.java
@@ -1,68 +1,55 @@
 package malenia;
 import malenia.task.*;
+import java.util.ArrayList;
 
 public class TaskList {
-    private Task[] list;
-    private int numOfItems;
-    private static final int MAX_TASKS = 100;
+    private ArrayList<Task> list;
 
-    // Create a Task List with a maximum of 100 items
+    // Create a Task List
     public TaskList() {
-        list = new Task[MAX_TASKS];
-        numOfItems = 0;
+        list = new ArrayList<>();
     }
 
-    // Add a new Todo item to the end of the task list
+    // Add a new Task item to the end of the task list
     public void add(Task task) {
-        if (numOfItems == list.length) {
-            System.out.println("Task list is full! Please remove some tasks before adding new ones.");
-            return;
-        }
-
-        list[numOfItems] = task;
-        numOfItems++;
+        list.add(task);
     }
 
     // Remove an item from the task list by its index
     public void remove(int index) {
-        if (index < 0 || index >= numOfItems) {
+        if (index < 0 || index >= list.size()) {
             System.out.println("Invalid index! Please provide a valid task index to remove.");
             return;
         }
 
-        for (int i = index; i < numOfItems - 1; i++) {  // Shift items to the left at the removal point
-            list[i] = list[i + 1];
-        }
-
-        list[numOfItems - 1] = null;
-        numOfItems--;
+        list.remove(index);
     }
 
     // Return the number of items in the task list
     public int getNumOfItems() {
-        return numOfItems;
+        return list.size();
     }
 
     public Task getTask(int index) {
-        if (index < 0 || index >= numOfItems) {
+        if (index < 0 || index >= list.size()) {
             System.out.println("Invalid index! Please provide a valid task index.");
             return null;
         }
 
-        return list[index];
+        return list.get(index);
     }
 
     // Print all items in the task list
     public void printList() {
-        if (numOfItems == 0) {
+        if (list.size() == 0) {
             Messages.wrapWithBars("Your task list is empty!");
             return;
         }
 
         System.out.println(Messages.STANDARD_BAR);
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < numOfItems; i++) {
-            System.out.println((i + 1) + "." + list[i]);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + "." + list.get(i));
         }
 
         System.out.println(Messages.STANDARD_BAR);
